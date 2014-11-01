620a6e18872e441b089a36ce385b5f0dac9abbc9
Juho Kim <truepanic@gmail.com>
2014-11-01 10:58:21 -0400

    automatic pause

--- vid-learning-ui/javascripts/app.js
+++ vid-learning-ui/javascripts/app.js
@@ -5,15 +5,55 @@ var model = {
 
 var pageApp = angular.module("page-app", []);
 
+
+/* Handling parameters for MTurk */
+var prmstr = window.location.search.substr(1);
+var prmarr = prmstr.split("&");
+var params = {};
+for ( var i = 0; i < prmarr.length; i++) {
+    var tmparr = prmarr[i].split("=");
+    params[tmparr[0]] = tmparr[1];
+}
+
+var id = params["id"];
+var isPreview = false;
+if (typeof params["assignmentId"] === "undefined" || params["assignmentId"] == "ASSIGNMENT_ID_NOT_AVAILABLE")
+    isPreview = true;
+if (typeof params["preview"] !== "undefined" && params["preview"] == 0)
+    isPreview = false;
+
+var workerId = "";
+if (typeof params["workerId"] !== "undefined")
+    workerId = params["workerId"];
+
+var vid = "2NIG1bt8aPU"; // default video ID
+if (params["id"]) {
+    vid = params["id"]
+}
+
+var timestamps = [{"start": 154, "end": 290}];
+var player;
+
+function updatePlayerInfo(){
+  for (var i in timestamps) {
+    var peak = timestamps[i];
+    // only trigger the first time (video not paused)
+    if (player.getPlayerState() != 2 && parseInt(player.getCurrentTime()) == parseInt(peak["end"])) {
+      console.log("End of a peak, prompt!");
+    }
+  }
+}
+
 var createPlayer = function () {
-  var player = new YT.Player('player', {
+  player = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId: 'M7lc1UVf-VE',
+    videoId: vid,
     events: {
       'onReady': function (event) {
         event.target.playVideo();
         //    player.stopVideo();
+        setInterval(updatePlayerInfo, 1000);
       },
       'onStateChange': function (event) {
         if (event.data == YT.PlayerState.PLAYING) {
